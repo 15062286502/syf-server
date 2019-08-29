a35d2561d450ba0ff25cfcf29a5515ace37b2385
songyufeng <songyufeng@glaway.com>
2019-08-29 13:56:47 +0800

    8.29

---
+++ src/main/java/com/example/syfserver/SyfServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.syfserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyfServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyfServerApplication.class, args);
+    }
+
+}
