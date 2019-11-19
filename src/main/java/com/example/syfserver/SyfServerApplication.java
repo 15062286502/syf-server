67fe943d82f1671e17adc5df7c50c5c3d5ff1c8d
songyufeng <songyufeng@glaway.com>
2019-11-19 14:10:11 +0800

    11.19

--- src/main/java/com/example/syfserver/SyfServerApplication.java
+++ src/main/java/com/example/syfserver/SyfServerApplication.java
@@ -2,12 +2,18 @@ package com.example.syfserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SyfServerApplication {
+public class SyfServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SyfServerApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SyfServerApplication.class);
     }
 
 }
