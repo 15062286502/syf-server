44dc5cbd7e78d34fca8579c0032d8f748a1b0955
songyufeng <songyufeng@glaway.com>
2019-12-20 15:20:36 +0800

    12.20

---
+++ src/main/java/com/example/syfserver/configuartion/WebConfigurer.java
@@ -0,0 +1,15 @@
+package com.example.syfserver.configuartion;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfigurer implements WebMvcConfigurer {
+    @Autowired
+    private Interceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/home/login");
+    }
+}
