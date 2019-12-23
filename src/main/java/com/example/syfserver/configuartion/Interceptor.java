31a22e5ca1c1beb5f76a96ba5ad59fb5c433f0ca
songyufeng <songyufeng@glaway.com>
2019-12-23 18:00:00 +0800

    12.2

--- src/main/java/com/example/syfserver/configuartion/Interceptor.java
+++ src/main/java/com/example/syfserver/configuartion/Interceptor.java
@@ -13,25 +13,27 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class Interceptor implements HandlerInterceptor {
     @Resource
-    private ValueOperations<String,Object> valueOperations;
+    private ValueOperations<String, Object> valueOperations;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("Authorization").toString();
-        if (valueOperations.get(token)==null){
-            String origin = request.getHeader("Origin");
-            response.setHeader("Access-Control-Allow-Origin", origin);
-            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-            response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
-            response.setHeader("Access-Control-Allow-Credentials", "true");
-            response.setCharacterEncoding("UTF-8");
-            response.setContentType("application/json; charset=utf-8");
-            try{
+        if (valueOperations.get(token) == null) {
+
+            try {
+                String origin = request.getHeader("Origin");
+                response.setHeader("Access-Control-Allow-Origin", origin);
+                response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+                response.setHeader("Access-Control-Max-Age", "3600");
+                response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
+                response.setHeader("Access-Control-Allow-Credentials", "true");
+                response.setCharacterEncoding("UTF-8");
+                response.setContentType("application/json; charset=utf-8");
                 JSONObject json = new JSONObject();
-                json.put("msg","登录过期");
-                json.put("code","50000");
+                json.put("msg", "登录过期");
+                json.put("code", "50000");
                 response.getWriter().append(json.toJSONString());
-            }catch (Exception e){
+            } catch (Exception e) {
                 response.sendError(500);
                 return false;
             }
